c0783ef4c3deb8b0b3d38b14bd84a49b89c215f7
sandra angeard <s.angeard@gmail.com>
2014-01-14 15:44:31 +0100

    Ajout de tout les sources existantes

---
+++ doc/HTMLSmartyConverter/default/04c3d2dbbf489b6a0a91960cec894818/%%239^%%239105369^footer.tpl.php
@@ -0,0 +1,12 @@
+<?php /* Smarty version 2.6.0, created on 2014-01-14 14:36:07
+         compiled from footer.tpl */ ?>
+	<div id="credit">
+		<hr>
+		Documentation generated on <?php echo $this->_tpl_vars['date']; ?>
+ by <a href="<?php echo $this->_tpl_vars['phpdocwebsite']; ?>
+">phpDocumentor <?php echo $this->_tpl_vars['phpdocversion']; ?>
+</a>
+	</div>
+</div>
+</body>
+</html>
